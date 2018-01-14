21069de186ece1f5d14794e536be4569c5bc9836
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-01-15 00:47:54 +0600

    created a new admin controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public public function __construck()
+    {
+
+    }
+
+    public public function index($value='')
+    {
+    	return view('admin.home');
+    }
+}
